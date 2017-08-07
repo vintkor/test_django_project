2ff48959eb21500d16b59c2cdf53efd855e6a207
Alekxandr <alkv84@yandex.ru>
2017-08-07 14:47:58 +0300

    Import is work

--- catalog/resources.py
+++ catalog/resources.py
@@ -1,8 +1,14 @@
-from import_export.resources import ModelResource
-from catalog.models import CatalogProduct
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
+from catalog.models import CatalogProduct, CatalogCategory
 
 
-class CatalogProductResource(ModelResource):
+class CatalogProductResource(resources.ModelResource):
+    category = fields.Field(
+        column_name='category',
+        attribute='category',
+        widget=ForeignKeyWidget(CatalogCategory, 'id')
+    )
 
     class Meta:
         model = CatalogProduct
